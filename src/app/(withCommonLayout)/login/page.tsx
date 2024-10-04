0d61ebb6b133829224ec220e245a85e8dd43114d
Forhad96 <forhadairdrop@gmail.com>
2024-10-04 09:56:29 +0600

    Feat:login and resgister user ui and functionality done

---
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+import { useUserLogin } from "@/src/hooks/auth.hook";
+
+
+
+import loginValidationSchema from "@/src/schemas/login.schema";
+import { useUser } from "@/src/context/user.context";
+import CommonForm from "@/src/components/form/CommonForm";
+import CommonInput from "@/src/components/form/CommonInput";
+import Loading from "@/src/components/ui/Loading";
+
+const LoginPage = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const { setIsLoading: userLoading } = useUser();
+
+  const redirect = searchParams.get("redirect");
+
+  const { mutate: handleUserLogin, isPending, isSuccess } = useUserLogin();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    handleUserLogin(data);
+    userLoading(true);
+  };
+
+  useEffect(() => {
+    if (!isPending && isSuccess) {
+      if (redirect) {
+        router.push(redirect);
+      } else {
+        router.push("/");
+      }
+    }
+  }, [isPending, isSuccess]);
+
+  return (
+    <>
+      {isPending && <Loading />}
+      <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
+        <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
+        <p className="mb-4">Welcome Back! Let&lsquo;s Get Started</p>
+        <div className="w-[35%]">
+          <CommonForm
+            resolver={zodResolver(loginValidationSchema)}
+            onSubmit={onSubmit}
+          >
+            <div className="py-3">
+              <CommonInput label="Email" name="email" type="email" />
+            </div>
+            <div className="py-3">
+              <CommonInput label="Password" name="password" type="password" />
+            </div>
+
+            <Button
+              className="my-3 w-full rounded-md bg-default-900 font-semibold text-default"
+              size="lg"
+              type="submit"
+            >
+              Login
+            </Button>
+          </CommonForm>
+          <div className="text-center">
+            Don&lsquo;t have account ? <Link href={"/register"}>Register</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
